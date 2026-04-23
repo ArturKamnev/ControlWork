7d83de5db0486868ad4af64ad59e948acf096045
ArturKamnev <kamartur778@gmail.com>
2026-04-23 18:55:07 +0600

    Контрольная

--- oop.py
+++ oop.py
@@ -1,43 +1,45 @@
-# class Person:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.__age__ = age
-#
-#     def get_age(self):
-#         return self.__age__
-#
-#     def set_age(self, new_age):
-#         if new_age > 0:
-#             self.__age__ = new_age
-#         else:
-#             raise ValueError("Вы ввели неправильное значение")
-#
-#
-# p = Person("Artur", 25)
-# p.set_age(25)
-# print(p.get_age())
-# p.set_age(-5)
-#
-# class Animal:
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def speak(self):
-#         return "I am an animal"
-#
-#
-# class Dog(Animal):
-#     def speak(self):
-#         return "Woof"
-#
-# class Cat(Animal):
-#     def speak(self):
-#         return "Meow"
-#
-# dog = Dog("Buddy")
-# cat = Cat("Kitty")
-# print(dog.name, dog.speak())
-# print(cat.name, cat.speak())
+from abc import ABC, abstractmethod
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age__ = age
+
+    def get_age(self):
+        return self.__age__
+
+    def set_age(self, new_age):
+        if new_age > 0:
+            self.__age__ = new_age
+        else:
+            raise ValueError("Вы ввели неправильное значение")
+
+
+p = Person("Artur", 25)
+p.set_age(25)
+print(p.get_age())
+p.set_age(-5)
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "I am an animal"
+
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+
+dog = Dog("Buddy")
+cat = Cat("Kitty")
+print(dog.name, dog.speak())
+print(cat.name, cat.speak())
 
 class Vehicle:
     def move(self):
@@ -55,3 +57,28 @@ car = Car()
 bike = Bicycle()
 print(car.move())
 print(bike.move())
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        ...
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+rect = Rectangle(10, 5)
+circle = Circle(7)
+print(rect.area())
+print(circle.area())
